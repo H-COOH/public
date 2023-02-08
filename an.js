28d6418bbb7bdae4d19b2f53d686e7e8b53202a4
H-COOH <67232916+H-COOH@users.noreply.github.com>
2023-02-08 12:56:47 +0800

    add course

---
+++ an.js
@@ -0,0 +1,12 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://www.googletagmanager.com/gtag/js?id=G-FJYZJNEDXV";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-FJYZJNEDXV');
